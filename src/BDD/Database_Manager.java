c115658214ad09e4db67f973b9b6a4296bc03900
Labadi Abdessamie <star1coach@gmail.com>
2025-01-11 16:09:34 +0100

    Solving the Issue of SQL Injections

--- src/BDD/Database_Manager.java
+++ src/BDD/Database_Manager.java
@@ -10,8 +10,7 @@ import java.util.ArrayList;
 
 public class Database_Manager {
     Connection conn;
-    Statement statement;
-    String sql;
+    PreparedStatement sql;
     ResultSet Result;
     
     
@@ -29,13 +28,19 @@ public class Database_Manager {
     }
     
     public int ClientVerification(String Fname,String Lname,String Email) throws SQLException{
-        statement = conn.createStatement();
-        sql = "select Id_Client FROM Client where Nom='"+Lname+"' and Prenom='"+Fname+"' and Email='"+Email+"'";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT Id_Client FROM Client where Nom=? AND Prenom=? AND Email=?");
+            sql.setString(1, Lname);
+            sql.setString(2, Fname);
+            sql.setString(3, Email);
+        Result = sql.executeQuery();
+        
         if(!Result.isBeforeFirst()){
                 // Adding not a Registered Client
-                sql = "insert into client(Nom,Prenom,Email) VALUES ('" +Lname+ "','" +Fname+ "','" + Email +"')";
-            int execute = statement.executeUpdate(sql);
+            sql =conn.prepareStatement("INSERT INTO client(Nom,Prenom,Email) VALUES (?,?,?)");
+                sql.setString(1, Lname);
+                sql.setString(2, Fname);
+                sql.setString(3, Email);
+            int execute = sql.executeUpdate();
             if(execute > 0){
                 System.out.println("New Client Added Successfully");
                 return ClientVerification(Fname, Lname, Email);
@@ -50,9 +55,8 @@ public class Database_Manager {
     
     public ArrayList<Vols> DisplayAllVols() throws SQLException{
         ArrayList <Vols> Vols = new ArrayList<>();
-        statement = conn.createStatement();
-        sql = "SELECT * FROM vol";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT * FROM vol");
+        Result = sql.executeQuery();
         
         if(!Result.isBeforeFirst()){
             return Vols;
@@ -75,10 +79,12 @@ public class Database_Manager {
     
     public ArrayList<Vols> VolVerification(String origin,String destination,String DateD) throws SQLException{
         ArrayList <Vols> Vols = new ArrayList<>();
-        statement = conn.createStatement();
-        sql = "SELECT Num_Vol,DateDepart,DateArrive,empty_seats FROM vol WHERE Depart='"+origin+
-                "' and Arrive='"+destination+"' and DateDepart>='"+DateD+"'";
-        Result = statement.executeQuery(sql);
+        
+        sql = conn.prepareStatement("SELECT Num_Vol,DateDepart,DateArrive,empty_seats FROM vol WHERE Depart=? AND Arrive=? AND DateDepart>=?");
+                sql.setString(1, origin);
+                sql.setString(2, destination);
+                sql.setString(3, DateD);
+        Result = sql.executeQuery();
         if(!Result.isBeforeFirst()){
             return Vols;
         }
@@ -100,49 +106,57 @@ public class Database_Manager {
     }
     
     public boolean Make_Reservation(int Id_passager,String Number_Plane,String Date) throws SQLException{
-        statement = conn.createStatement();
-    
-        sql = "SELECT * FROM Client WHERE Id_client = '"+Id_passager+"'";
-        Result = statement.executeQuery(sql);
+        
+        sql = conn.prepareStatement("SELECT * FROM Client WHERE Id_client = ?");
+            sql.setInt(1, Id_passager);
+        Result = sql.executeQuery();
         Result.next();
-        sql = "INSERT INTO Passager (Id_Passager,Nom,Prenom,Email) VALUES ('"
-                + Result.getInt("Id_Client")+"','"+Result.getString("Nom")+"','"+Result.getString("Prenom")+"','"
-                + Result.getString("Email")+"')";
-        
-        int InsertedPassager = statement.executeUpdate(sql);
+        sql = conn.prepareStatement("INSERT INTO Passager (Id_Passager,Nom,Prenom,Email) VALUES (?,?,?,?)");
+                sql.setInt(1, Result.getInt("Id_Client"));
+                sql.setString(2, Result.getString("Nom"));
+                sql.setString(3, Result.getString("Prenom"));
+                sql.setString(4, Result.getString("Email"));
                 
-        sql = "INSERT INTO reservation (Num_Vol,Id_Passager,date_Reservation) VALUES ('"+Number_Plane+"','"+Id_passager+"','"+Date+"')";
-        int InsertedRow = statement.executeUpdate(sql);
+        int InsertedPassager = sql.executeUpdate();
+                
+        sql = conn.prepareStatement("INSERT INTO reservation (Num_Vol,Id_Passager,date_Reservation) VALUES (?,?,?)");
+            sql.setString(1, Number_Plane);
+            sql.setInt(2, Id_passager);
+            sql.setString(3, Date);
+        int InsertedRow = sql.executeUpdate();
         
-        sql = "UPDATE Vol Set empty_seats = empty_seats - 1 Where Num_Vol = "+Number_Plane;
-        int execute = statement.executeUpdate(sql);
+        sql = conn.prepareStatement("UPDATE Vol Set empty_seats = empty_seats - 1 WHERE Num_Vol = ?");
+            sql.setString(1, Number_Plane);
+        int execute = sql.executeUpdate();
         
         return InsertedRow > 0 && execute > 0 && InsertedPassager > 0;
     }
     
     public boolean Have_Reservation(int Id_passager) throws SQLException{
-        statement = conn.createStatement();
-        sql = "Select Num_Vol FROM reservation WHERE Id_Passager ='"+Id_passager+"'";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT Num_Vol FROM reservation WHERE Id_Passager =?");
+            sql.setInt(1, Id_passager);
+        Result = sql.executeQuery();
         if(!Result.isBeforeFirst()){
-             sql = "DELETE FROM Passager WHERE Id_Passager ='"+Id_passager+"'";
-            int execute = statement.executeUpdate(sql);
+            sql = conn.prepareStatement("DELETE FROM Passager WHERE Id_Passager =?");
+                sql.setInt(1, Id_passager);
+            int execute = sql.executeUpdate();
             return execute > 0;
         }
             return true;
     }
     
     /*public int getIndex() throws SQLException{
-        statement = conn.createStatement();
-        sql = "SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = 'historique' AND table_schema = 'airalgerie'";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT AUTO_INCREMENT FROM information_schema.tables WHERE table_name = ? AND table_schema = ?");
+            sql.setString(1,"historique");
+            sql.setString(2,"airalgerie");
+        Result = sql.executeQuery();
         Result.next();
         return Integer.parseInt(Result.getString(1));
     }*/
     public String SearchSender(int Id) throws SQLException {
-        statement = conn.createStatement();
-        sql = "SELECT Nom,Prenom FROM Client WHERE Id_Client='"+Id+"'";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT Nom,Prenom FROM Client WHERE Id_Client=?");
+            sql.setInt(1, Id);
+        Result = sql.executeQuery();
         Result.next();
         return (Result.getString("Nom")+" "+Result.getString("Prenom"));
     }
@@ -150,9 +164,9 @@ public class Database_Manager {
         
         ArrayList<ArrayList<String>> Clients = new ArrayList<>();
         
-        statement = conn.createStatement();
-        sql = "SELECT * FROM Client WHERE Id_Client!='"+Id_Conncted+"'";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT * FROM Client WHERE Id_Client!=?");
+            sql.setInt(1, Id_Conncted);
+        Result = sql.executeQuery();
         
         if(!Result.isBeforeFirst()){
             return Clients;
@@ -171,11 +185,13 @@ public class Database_Manager {
     public ArrayList<ArrayList<String>> GetHistory(int Client_1,int Client_2) throws SQLException{
         ArrayList<ArrayList<String>> OldMessages = new ArrayList<>();
         
-        statement = conn.createStatement();
-        sql = "SELECT Sender,Content,SentDate FROM Historique WHERE (Sender='"
-                + Client_1 +"' and Receiver ='"+Client_2+"')"
-                + " OR (Sender='"+Client_2+"' and Receiver ='" +Client_1+"')";
-        Result = statement.executeQuery(sql);
+        sql = conn.prepareStatement("SELECT Sender,Content,SentDate FROM Historique WHERE "
+                + "(Sender=? AND Receiver =?) OR (Sender=? AND Receiver =?)");
+                sql.setInt(1, Client_1);
+                sql.setInt(2, Client_2);
+                sql.setInt(3, Client_2);
+                sql.setInt(4, Client_1);
+        Result = sql.executeQuery();
         
         if(!Result.isBeforeFirst()){
             return OldMessages;
@@ -192,9 +208,11 @@ public class Database_Manager {
         }
     }
     public int SaveMessage(int Source,int Destination,String Content) throws SQLException{
-        statement = conn.createStatement();
-        sql = "INSERT INTO historique (Sender,Receiver,Content) VALUES ('"+Source+"','"+Destination+"','"+Content+"')";
-        int InsertedRow = statement.executeUpdate(sql);
+        sql = conn.prepareStatement("INSERT INTO historique (Sender,Receiver,Content) VALUES (?,?,?)");
+            sql.setInt(1, Source);
+            sql.setInt(2, Destination);
+            sql.setString(3, Content);
+        int InsertedRow = sql.executeUpdate();
         return InsertedRow;
     }
 }
